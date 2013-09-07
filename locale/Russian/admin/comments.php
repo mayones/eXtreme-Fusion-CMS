8dc4223237bb4ad364ffa50aac3fe9235fcfb6a1
mayones <nk.vasiliev@gmail.com>
2013-09-07 12:56:33 -0700

    Update comments.php

--- locale/Russian/admin/comments.php
+++ locale/Russian/admin/comments.php
@@ -4,9 +4,9 @@ return array(
 	'Comments' => 'Комментарии',
 	'Remove comment?' => 'Удалить комментарии?',
 	'There are no comments.' => 'Нет комментариев.',
-	'Comment has been edited.' => 'Комментарий отредактирован.',
+	'Comment has been edited.' => 'Комментарий изменен.',
 	'Comment has been deleted.' => 'Комментарий удален.',
-	'Error! Comment has not been edited.' => 'Произошла ошибка! Комментарий не был отредактирован.',
+	'Error! Comment has not been edited.' => 'Произошла ошибка! Комментарий не был изменен.',
 	'Error! Comment has not been deleted.' => 'Произошла ошибка! Комментарий не был удален.',
 	'Error! Missing id.' => 'Произошла ошибка! Отсутствует идентификатор :id для таблицы comments.'
 );
