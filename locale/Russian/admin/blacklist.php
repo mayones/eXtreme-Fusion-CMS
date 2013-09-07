76bd56fa3e68ec5e470b4398ee87d8bbb5c0c1fe
mayones <nk.vasiliev@gmail.com>
2013-09-07 12:50:33 -0700

    Update blacklist.php

--- locale/Russian/admin/blacklist.php
+++ locale/Russian/admin/blacklist.php
@@ -1,7 +1,7 @@
 <?php defined('EF5_SYSTEM') || exit;
 
 return array(
-	//Tytuły
+	//Заголовки
 	'Blocked addresses' => 'Заблокированные адреса',
 	'Blacklist' => 'Черный список',
 	'Blocked address edition' => 'Редактирование заблокированного адреса',
@@ -16,20 +16,20 @@ return array(
 	'Error! Specified address already is in database.' => 'Произошла ошибка! Этот адрес уже отсутствует в базе данных.',
 	'Error! Entered data is incorrect. Please validate e-mail address or IP address.' => 'Произошла ошибка! Правильные данные не были введены, проверти правильность email или IP',
 	'Error! Missing id.' => 'Произошла ошибка! Отсутствует идентификатор :id для таблицы blacklist.',
-	'Error! E-mail address can\'t be accepted with IP address. You must add IP address or e-mail address.' => 'Произошла ошибка! Nie można zatwierdzić adresu e-mail wraz z adresem IP, добавьте адрес IP или e-mail.',
+	'Error! E-mail address can\'t be accepted with IP address. You must add IP address or e-mail address.' => 'Произошла ошибка! Нельзя принять адрес e-mail вместе с адресом IP, добавьте тольк адрес IP или только e-mail.',
 
-	//Informacje
+	//Информация
 	'N/A' => 'Недоступные',
 	'Blacklist' => 'Черный список',
-	'Blacklist description' => '<p>Добавление адреса IP в черный список заблокирует пользователю с таким IP доступ к этому сайту. Можете вписать полный адрес IP, например <em>123.123.123.123</em>, или только его часть, например <em>123.123.123</em> или <em>123.123</em>.</p><p>Помните! Адреса IPv6 записываются в полной форме, например <em>ABCD:1234:5:6:7:8:9:FF</em> будет показан как <em>ABCD:1234:0005:0006:0007:0008:0009:00FF</em>. Смешанные адреса, частично содержащие данные из IPv6 и IPv4 не будут проверены.</p><p>Добавление в черный список адреса e-mail запретит пользователю с данным e-mail адресом регистрацию на сайте. Можете ввести полный адрес, например <em>foo@bar.com</em> или домен, владельцам адресов e-mail козапретить регистрацию, например <em>bar.com</em></p>',
+	'Blacklist description' => '<p>Добавление адреса IP в черный список заблокирует пользователю с таким IP доступ к этому сайту. Можете вписать полный адрес IP, например <em>123.123.123.123</em>, или только его часть, например <em>123.123.123</em> или <em>123.123</em>.</p><p>Помните! Адреса IPv6 записываются в полной форме, например <em>ABCD:1234:5:6:7:8:9:FF</em> будет показан как <em>ABCD:1234:0005:0006:0007:0008:0009:00FF</em>. Смешанные адреса, частично содержащие данные из IPv6 и IPv4 не будут проверены.</p><p>Добавление в черный список адреса e-mail запретит пользователю с данным e-mail адресом регистрацию на сайте. Можете ввести полный адрес, например <em>foo@bar.com</em> или домен, владельцам адресов e-mail которого вы хотите запретить регистрацию, например <em>bar.com</em></p>',
 
-	//Formularz
-	'Block IP address:' => 'Adres IP do zablokowania: <br /><br /><strong>lub</strong>',
-	'Block e-mail address:' => 'Adres e-mail do zablokowania:',
-	'Block reason:' => 'Powód:',
+	//Форма
+	'Block IP address:' => 'Адрес IP: <br /><br /><strong>или</strong>',
+	'Block e-mail address:' => 'Адрес e-mail:',
+	'Block reason:' => 'Причина:',
 
-	//Treść
-	'Address:' => 'Adres:',
-	'Blacklist is empty.' => 'Brak adresów na czarnej liście.'
+	//Содержание
+	'Address:' => 'Адрес:',
+	'Blacklist is empty.' => 'Черный список пуст.'
 
 );
