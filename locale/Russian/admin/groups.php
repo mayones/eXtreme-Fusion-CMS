f14b2fc2fb6e1dd1eaf7608dedd9a1c04e88e89f
mayones <nk.vasiliev@gmail.com>
2013-09-07 13:56:33 -0700

    Create groups.php

---
+++ locale/Russian/admin/groups.php
@@ -0,0 +1,22 @@
+<?php defined('EF5_SYSTEM') || exit;
+
+return array(
+	'Groups permissions' => 'Uprawnienia grup',
+	'Group' => 'Группа',
+	'Group: :name' => 'Группа: :name',
+	'Add group' => 'Создать группы',
+	'Edit group' => 'Редактирование группы',
+	'Add new' => 'Создать новую',
+	'Show on team page:' => 'Wyświetlać grupę na podstronie zespołu:',
+	'All permissions' => 'Wszystkie uprawnienia',
+	'Group has been added.' => 'Группа добавлена.',
+	'Group has been edited.' => 'Grupa została zmodyfikowana.',
+	'Group has been deleted.' => 'Группа удалена.',
+	'Error! Group has not been edited.' => 'Произошла ошибка! Группа не была отредактирована.',
+	'Error! Group has not been added.' => 'Произошла ошибка! Группа не была добавлена.',
+	'Error! Group has not been deleted.' => 'Произошла ошибка! Группа не была удалена.',
+	'Error! There are no records in permissions table.' => 'Произошла ошибка! Отсутствуют записи в таблице permissions.',
+	'Error! There are no records in permissions_sections table.' => 'Произошла ошибка! Отсутствуют записи в таблице permissions_sections.',
+	'You can not delete system group.' => 'Вы не можете удалить системную группу.',
+	'About all permissions' => 'Zaznaczenie tej opcji spowoduje, że użytkownicy należący do tej grupy będą posiadać na bieżąco wszystkie uprawnienia.',
+);
