19ea3deba6dd990be5d7e8d20a61ed4efd2e05ad
mayones <nk.vasiliev@gmail.com>
2013-09-07 12:09:21 -0700

    Create bbcodes.php

---
+++ locale/Russian/admin/bbcodes.php
@@ -0,0 +1,16 @@
+<?php defined('EF5_SYSTEM') || exit;
+
+return array(
+	'BBCodes' => 'Теги BBCode',
+	'Active BBCodes' => 'Включеные теги BBCode',
+	'Inactive BBCodes' => 'Выключеные теги BBCode',
+	'Icon:' => 'Иконка:',
+	'No BBCode tags' => 'Теги BBCode отсутствуют',
+	'Active' => 'Включить',
+	'Unactive' => 'Выключить',
+	//Logs
+	'Tag has been deactivated' => 'Тег был выключен',
+	'Error while deactivating' => 'Во время выключения произошла ошибка',
+	'Tag has been activated' => 'Тег был включен',
+	'Error while activating' => 'Во время включения произошла ошибка'
+);
