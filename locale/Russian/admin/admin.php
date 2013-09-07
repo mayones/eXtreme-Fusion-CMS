04269da1957720db2ac3f3b4249ac7585a0e6e2b
mayones <nk.vasiliev@gmail.com>
2013-09-07 11:50:43 -0700

    Create admin.php

---
+++ locale/Russian/admin/admin.php
@@ -0,0 +1,15 @@
+<?php defined('EF5_SYSTEM') || exit;
+
+return array(
+	'Permissions' => 'Права',
+	'Roles' => 'Права',
+	'Time to end of session' => 'Время до конца сессии',
+	'Manage content' => 'Управление содержанием',
+	'Manage users' => 'Управление пользователями',
+	'Manage site' => 'Управление сайтом',
+	'Settings' => 'Настройки',
+	'Modules' => 'Модули',
+	'There are no navigation links.' => 'Ссылки навигации отстутсвуют.',
+	// Site Information
+	'Statistics' => 'Статистика сайта',
+);
