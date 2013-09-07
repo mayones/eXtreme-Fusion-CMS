ec9513cad6d6401ca21f9d367ce89f58fda2d498
mayones <nk.vasiliev@gmail.com>
2013-09-07 12:55:12 -0700

    Create comments.php

---
+++ locale/Russian/admin/comments.php
@@ -0,0 +1,12 @@
+<?php defined('EF5_SYSTEM') || exit;
+
+return array(
+	'Comments' => 'Комментарии',
+	'Remove comment?' => 'Удалить комментарии?',
+	'There are no comments.' => 'Нет комментариев.',
+	'Comment has been edited.' => 'Комментарий отредактирован.',
+	'Comment has been deleted.' => 'Комментарий удален.',
+	'Error! Comment has not been edited.' => 'Произошла ошибка! Комментарий не был отредактирован.',
+	'Error! Comment has not been deleted.' => 'Произошла ошибка! Комментарий не был удален.',
+	'Error! Missing id.' => 'Произошла ошибка! Отсутствует идентификатор :id для таблицы comments.'
+);
