34e46f0402c736d79439a13207196d1431840c36
mayones <nk.vasiliev@gmail.com>
2013-09-07 13:51:20 -0700

    Create favourits.php

---
+++ locale/Russian/admin/favourits.php
@@ -0,0 +1,7 @@
+<?php defined('EF5_SYSTEM') || exit;
+
+return array(
+	'Favourites' => 'Избранное',
+	'Update is available for this version of eXtreme-Fusion CMS.' => 'Для этой версии eXtreme-Fusion CMS доступно обновление.',	
+	'You do not have favourite items.' => 'Нет избранных страниц.'
+);
