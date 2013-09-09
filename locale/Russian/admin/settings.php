1e6fbf57f98c3a0c3263f401cf7e07cbf13f5958
Nikolay Vasilyev <nk.vasiliev@gmail.com>
2013-09-09 12:03:05 -0700

    Create settings.php

---
+++ locale/Russian/admin/settings.php
@@ -0,0 +1,33 @@
+<?php defined('EF5_SYSTEM') || exit;
+
+return array(
+	//Site Settings
+	'General' => 'Главное',
+	'Site name:' => 'Название сайта:',
+	'Site description:' => 'Описание:',
+	'Site URL:' => 'Адресс сайта:',
+	'Banner:' => 'Лого:',
+	'This banner will be displayed if only design author will use correct template methods.' => 'Изображение будет использовано в случе если автор шаблона предусмотрел загрузку изображения в это поле..',
+	'Admin e-mail address:' => 'E-mail администратора:',
+	'Your nick:' => 'Логин:',
+	'Welcome message:' => 'Сообщение:',
+	'Optional, field can be empty. HTML code is allowed.' => 'Это поле может остаться пустым. Разрешено использование HTML.',
+	'Site Description' => 'Описание:',
+	'Separate keywords by comma.' => 'Ключевые слова отделяйте запятой.',
+	'Footer:' => 'Подвал:',
+	'HTML code is not allowed.' => 'Запрещено использование HTML.',
+	'Set as a homepage:' => 'Сделать стартовой страницей:',
+	'News style:' => 'Стили новостей:',
+	'Single Column' => 'одна колонка',
+	'Double Column' => 'две колонки',
+	'Enable language detection by browser?' => 'Включить определение языка?',
+	'Default language:' => 'Язык сайта:',
+	'Default theme:' => 'Тема:',
+	'Default search place:' => 'Стандартно искать в:',
+	//Logs
+	'The language chosen is not avaible.' => 'Выбранный язык недоступен.',
+	'The template chosen is not available.' => 'Выбранная тема недотупна.',
+	'Data has been saved. Please refresh the page to see changes.' => 'Данные сохранены. Обновите страницу, чтобы увидеть изменения.',
+	'System language has been changed.' => 'Язык системы изменен.',
+
+);
