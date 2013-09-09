56eea9abc2146889e4cbae342eb1c52e075df018
Nikolay Vasilyev <nk.vasiliev@gmail.com>
2013-09-09 06:58:51 -0700

    Update groups.php

--- locale/Russian/admin/groups.php
+++ locale/Russian/admin/groups.php
@@ -1,16 +1,16 @@
 <?php defined('EF5_SYSTEM') || exit;
 
 return array(
-	'Groups permissions' => 'Uprawnienia grup',
+	'Groups permissions' => 'Права групп',
 	'Group' => 'Группа',
 	'Group: :name' => 'Группа: :name',
 	'Add group' => 'Создать группы',
 	'Edit group' => 'Редактирование группы',
 	'Add new' => 'Создать новую',
 	'Show on team page:' => 'Wyświetlać grupę na podstronie zespołu:',
-	'All permissions' => 'Wszystkie uprawnienia',
+	'All permissions' => 'Все права',
 	'Group has been added.' => 'Группа добавлена.',
-	'Group has been edited.' => 'Grupa została zmodyfikowana.',
+	'Group has been edited.' => 'Группа изменена.',
 	'Group has been deleted.' => 'Группа удалена.',
 	'Error! Group has not been edited.' => 'Произошла ошибка! Группа не была отредактирована.',
 	'Error! Group has not been added.' => 'Произошла ошибка! Группа не была добавлена.',
@@ -18,5 +18,5 @@ return array(
 	'Error! There are no records in permissions table.' => 'Произошла ошибка! Отсутствуют записи в таблице permissions.',
 	'Error! There are no records in permissions_sections table.' => 'Произошла ошибка! Отсутствуют записи в таблице permissions_sections.',
 	'You can not delete system group.' => 'Вы не можете удалить системную группу.',
-	'About all permissions' => 'Zaznaczenie tej opcji spowoduje, że użytkownicy należący do tej grupy będą posiadać na bieżąco wszystkie uprawnienia.',
+	'About all permissions' => 'Выбор этого пункта наделяет учатсникам все права.',
 );
