5eb197415f7a7b8200f3c537baeef122e88a7496
Nikolay Vasilyev <nk.vasiliev@gmail.com>
2013-09-09 07:21:13 -0700

    Create navigations.php

---
+++ locale/Russian/admin/navigations.php
@@ -0,0 +1,26 @@
+<?php defined('EF5_SYSTEM') || exit;
+
+return array(
+
+	'Add Navigation Link' => 'Добавить ссылку в навигацию',
+	'URL:' => 'Ссылка URL:',
+	'Position:' => 'Позиция:',
+	'Open in new window:' => 'Открыть в новом окне:',
+	'Vertical menu' => 'Вертикальное меню',
+	'Horizontal menu' => 'Горизонтальное меню',
+	'Vertical and horizontal menu' => 'Вертикальное и горизонтальное',
+	'Navigation' => 'Навигация',
+	'No site links.' => 'Нет ссылок в навигации.',
+
+	'Link has been added.' => 'Ссылка добавлена.',
+	'Link has been edited.' => 'Ссылка изменена.',
+	'Link has been deleted.' => 'Ссылка удалена.',
+	'Navigation has been refreshed' => 'Навигация обновлена.',
+	'Error! Link has not been added.' => 'Произошла ошибка! Ссылка не была добавлена.',
+	'Error! Link has not been edited.' => 'Произошла ошибка! Ссылка не была изменена.',
+	'Error! Link has not been deleted.' => 'Произошла ошибка! Ссылка не была удалена.',
+	'Error! Navigation has not been refreshed.' => 'Произошла ошибка! Навигация не была обновлена.',
+	'' => '',
+
+
+);
