cdc173185f8061a7e60a654c2cfe5ec454e276e8
Nikolay Vasilyev <nk.vasiliev@gmail.com>
2013-09-09 11:33:56 -0700

    Create permissions.php

---
+++ locale/Russian/admin/permissions.php
@@ -0,0 +1,29 @@
+<?php defined('EF5_SYSTEM') || exit;
+
+return array(
+	'Section:' => 'Секция:',
+	'ID:' => 'ID:',
+	'Permission' => 'Право',
+	'Permissions' => 'Права',
+	'Edit permission' => 'Изменить права',
+	'Edit section' => 'Редактировать секцию',
+	'Add new permission' => 'Добавить права',
+	'Add new section' => 'Добавить секцию',
+	'Add new' => 'Создать новое',
+	'Permission has been edited.' => 'Право отредактировано.',
+	'Permission has been added.' => 'Право добавлено.',
+	'Permission has been deleted.' => 'Право удалено.',
+	'You can not delete system permission!' => 'Вы не можете удалить системное право!',
+	'Error! Permission has not been edited.' => 'Произошла ошибка! Право не было изменено.',
+	'Error! Permission has not been added.' => 'Произошла ошибка! Право не было добавлено.',
+	'Error! Permission has not been deleted.' => 'Произошла ошибка! Право не было удалено.',
+	'Section has been edited.' => 'Секция изменена.',
+	'Section has been added.' => 'Секцтя добавлена.',
+	'Section has been deleted.' => 'Секция удалена.',
+	'You can not delete system section!' => 'Вы не можете удалить системную секцию!',
+	'This permission name is already used.' => 'Название уже используется.',
+	'Error! Section has not been edited.' => 'Произошла ошибка! Секция не была изменена.',
+	'Error! Section has not been added.' => 'Произошла ошибка! Секция не была добавлена.',
+	'Error! Section has not been deleted.' => 'Произошла ошибка! Секция не была удалена.',
+	'Remember' => 'Помните! Удаление секции повлечет удаление привязанных к ней прав.'
+);
