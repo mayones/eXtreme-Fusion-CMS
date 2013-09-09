0ba0b5fc56a963c882f237dcd019a0ef47291ff6
Nikolay Vasilyev <nk.vasiliev@gmail.com>
2013-09-09 07:07:53 -0700

    Create home.php

---
+++ locale/Russian/admin/home.php
@@ -0,0 +1,39 @@
+<?php defined('EF5_SYSTEM') || exit;
+
+return array(
+	//Informacyjne
+	'History' => 'История',
+	'Message:' => 'Сообщения:',
+	'Not&nbsp;plugged.' => 'Не&nbsp;подключено.',
+	'Download' => 'Download',
+	'Update' => 'Обновление',
+	'Notes' => 'Заметки',
+	'Quick&nbsp;news' => 'Быстрая&nbsp;новость',
+	'You have currently version of eXtreme-Fusion CMS.' => 'Ваша версия eXtreme-Fusion CMS не требует обновлений.',
+	'Update is available.' => 'Доступно обновление.',
+	'Quick news has been added.' => 'Быстрая новость добавлена.',
+	'Error! Quick news has not been added.' => 'Произогла ошибка! Быстрая новость не была добавлена.',
+	'Note has been added.' => 'Заметка добавлена.',
+	'Note has been edited.' => 'Заметка изменена.',
+	'Note has been deleted.' => 'Заметка удалена.',
+	'Error! Note has not been edited.' => 'Произогла ошибка! Заметка не была изменена.',
+	'Error! Note has not been added.' => 'Произогла ошибка! Заметка не была добавлена.',
+	'Error! Note has not been deleted.' => 'Произогла ошибка! Заметка не была удалена.',
+	'There are no logs.' => 'Записи отсутствуют.',
+	
+	//Заметки
+	'Click to edit...' => 'Нажми, чтобы редактировать...',
+	'Add new note' => 'Добавить заметку',
+	'Content:' => 'Содержание:',
+	'Block editing' => 'Заблокировать редактирование',
+	'Only author can edit this note.' => 'Только автор может редаквтировать эту .',
+	'You have created maximum number of notes. Remove some.' => 'Создано максимальное количество заметок. Удалите некоторые.',
+	'Added' => 'Добавлено',
+	'by' => 'автором',
+	
+	// Обновления
+	'Update is ready to install.' => 'Обновление готово к усатновке.',
+	'Update is available.' => 'Обновление доступно.',
+	'You have currently version of eXtreme-Fusion CMS.' => 'Ваша версия eXtreme-Fusion CMS не требует обновлений.',
+	'Updating error' => 'Вы не можете воспользоваться этим обновлением.<br /> Скачайте другое обновление, соответствующе вашей версии CMS.<br /> <a href="https://github.com/eXtreme-Fusion/EF5-updates">https://github.com/eXtreme-Fusion/EF5-updates</a>'
+);
