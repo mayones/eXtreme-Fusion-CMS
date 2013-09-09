431ce7c9d03012bf04eaeb12a59fde472fb43b7d
Nikolay Vasilyev <nk.vasiliev@gmail.com>
2013-09-09 07:11:26 -0700

    Create logs.php

---
+++ locale/Russian/admin/logs.php
@@ -0,0 +1,24 @@
+<?php defined('EF5_SYSTEM') || exit;
+
+return array(
+	'Logs' => 'Дневник событий',
+	'There are no logs.' => 'Записи отсутствуют.',
+	'Logs list' => 'Список событий',
+	'Action:' => 'Действие:',
+	'File:' => 'Файл:',
+	'add' => 'добавлено',
+	'edit' => 'редавтировано',
+	'delete' => 'удалено',
+	'The log has been deleted.' => 'Запись удалена.',
+	'Error! The log has not been deleted.' => 'Произогла ошибка! Запись не была удалена.',
+	'All logs have been deleted.' => 'Все записи удалены.',
+	'Error! Logs have not been deleted.' => 'Произошла ошибка! Записи не были удалены.',
+	'valid' => 'хорошо',
+	'error' => 'ошибка',
+	'---No Data---' => '---пусто---',
+	'Logs are enabled.' => 'Добавление записей в данный момент включено.',
+	'Logs are disabled.' => 'Добавление записей в данный момент выключено.'
+
+
+
+);
