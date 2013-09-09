c96d3d763e4569d459c390aa7c3cc00ef0e00c21
Nikolay Vasilyev <nk.vasiliev@gmail.com>
2013-09-09 11:14:34 -0700

    Create panel_editor.php

---
+++ locale/Russian/admin/panel_editor.php
@@ -0,0 +1,18 @@
+<?php defined('EF5_SYSTEM') || exit;
+
+return array(
+	// GENERAL
+	'Panel editor' => 'Редактор панелей',
+	'Content:' => 'Содержание:',
+	// PLACE
+	'Place:' => 'Положение:',
+	'Left side' => 'Слева',
+	'Right side' => 'Справа',
+	'Top' => 'Вверху',
+	'Bottom' => 'Внизу',
+	// MESSAGES
+	'The panel has been added.' => 'Панель добавлена.',
+	'Error! The panel has not been added.' => 'Произошла ошибка! Панель не была добавлена.',
+	'The panel has been updated.' => 'Панели изменена.',
+	'Error! The panel has not been updated.' => 'Произошла ошибка! Панель не была изменена.'
+);
