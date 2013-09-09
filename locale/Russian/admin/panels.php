1e940a4f69fe3bb18d043f6c5cd612b902a9b4e4
Nikolay Vasilyev <nk.vasiliev@gmail.com>
2013-09-09 11:17:33 -0700

    Create panels.php

---
+++ locale/Russian/admin/panels.php
@@ -0,0 +1,15 @@
+<?php defined('EF5_SYSTEM') || exit;
+
+return array(
+	'Panels managment' => 'Управление панелями',
+	'Add new panel' => 'Добавить панель',
+	'Delete this panel?' => 'Удалить панель?',
+	'File' => 'Файл',
+	'Module' => 'Модуль',
+	'Code' => 'Код',
+	'Panel type:' => '<i><strong>Тип панели:</i></strong>',
+	'Panel access:' => '<i><strong>Показать:</i></strong>',
+	'Inactive panels' => 'Неактивные панели',
+	'Panels on website' => 'Панели на сайте',
+	'Moving here' => 'Перенос панели в эту секцию повлечет за собой удаление панели из базы данных'
+);
