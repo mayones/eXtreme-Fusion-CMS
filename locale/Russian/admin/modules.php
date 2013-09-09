1f41a1a6a01ec52576e05b6da5d57041cffa543e
Nikolay Vasilyev <nk.vasiliev@gmail.com>
2013-09-09 07:15:11 -0700

    Create modules.php

---
+++ locale/Russian/admin/modules.php
@@ -0,0 +1,17 @@
+<?php defined('EF5_SYSTEM') || exit;
+
+return array(
+	'Module' => 'Модуль',
+	'Modules'  => 'Модули',
+	'Version:'  => 'Версия:',
+	'Update' => 'Обновление',
+	'Legend:' => 'Легенда:',
+	'Modules under Development.' => 'Модули находятся в разработке.',
+	'Installed Modules under Development.' => 'Установленные модули находящиеся в процессе разработки.',
+	'Modules officially released.' => 'Модуль официально опубликован.',
+	'Installed Modules officially released.' => 'Установленные официально опубликованные модули .',
+	'Modules have been installed.' => 'Модули установлены.',
+	'Modules have been uninstalled.' => 'Модули удалены.',
+	'Error! Modules have not been installed.' => 'Произошла ошибка! Модули не были установлены.',
+	'Error! Modules have not been uninstalled.' => 'Произошла ошибка! Модули не были удалены.',
+);
